8e4f7381f4567fea4c8d4d01f3d2c7fef1f35ab5
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-11-18 13:30:07 -0500

    Finished application

--- components/cart-summary.tsx
+++ components/cart-summary.tsx
@@ -7,10 +7,28 @@ import { formatCurrencyString, useShoppingCart } from "use-shopping-cart"
 import { Button } from "@/components/ui/button"
 
 export function CartSummary() {
-  const {formattedTotalPrice, totalPrice, cartDetails, cartCount} = useShoppingCart();
+  const { formattedTotalPrice, totalPrice, cartDetails, cartCount, redirectToCheckout } =
+    useShoppingCart()
   const shippingAmount = cartCount! > 0 ? 500 : 0
-  const totalAmount = totalPrice! + shippingAmount;
-  function onCheckout() {}
+  const totalAmount = totalPrice! + shippingAmount
+
+  const [isLoading, setIsLoading] = useState(false)
+  const isDisabled = isLoading || cartCount! === 0;
+
+  async function onCheckout() {
+    setIsLoading(true);
+    const response =  await fetch('/api/checkout', {
+      method: "POST",
+      body: JSON.stringify(cartDetails)
+    })
+    const data = await response.json();
+    const result = await redirectToCheckout(data.id)
+
+    if(result?.error) {
+      console.error(result);
+    }
+    setIsLoading(false);
+  }
 
   return (
     <section
@@ -30,18 +48,22 @@ export function CartSummary() {
           <dt className="flex items-center text-sm">
             <span>Shipping estimate</span>
           </dt>
-          <dd className="text-sm font-medium">{formatCurrencyString({currency: "CAD", value: shippingAmount})}</dd>
+          <dd className="text-sm font-medium">
+            {formatCurrencyString({ currency: "USD", value: shippingAmount })}
+          </dd>
         </div>
         <div className="flex items-center justify-between border-t border-gray-200 pt-4 dark:border-gray-600">
           <dt className="text-base font-medium">Order total</dt>
-          <dd className="text-base font-medium">{formatCurrencyString({currency: "CAD", value: totalAmount})}</dd>
+          <dd className="text-base font-medium">
+            {formatCurrencyString({ currency: "USD", value: totalAmount })}
+          </dd>
         </div>
       </dl>
 
       <div className="mt-6">
-        <Button className="w-full">
-          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          Loading...
+        <Button className="w-full" disabled={isDisabled} onClick={onCheckout}>
+          {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+          {isLoading ? "Loading..." : "Checkout"}
         </Button>
       </div>
     </section>
